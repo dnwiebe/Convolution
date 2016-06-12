714ba3aa31d5eef0156f71c8f608a048f1a2af53
Dan Wiebe <dnwiebe@gmail.com>
2016-06-12 11:44:43 -0400

    VestibuleService is now mostly an actor

--- app/services/VestibuleService.scala
+++ app/services/VestibuleService.scala
@@ -1,11 +1,12 @@
 package services
 
-import akka.actor.ActorRef
-import com.google.inject.Singleton
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import com.google.inject.{Inject, Singleton}
 
 import scala.collection.mutable.ListBuffer
-import utils.Utils._
+import akka.pattern.ask
 
+import scala.concurrent.duration._
 import scala.concurrent.Future
 import play.api.libs.concurrent.Execution.Implicits._
 
@@ -13,56 +14,98 @@ import play.api.libs.concurrent.Execution.Implicits._
   * Created by dnwiebe on 6/5/16.
   */
 
-@Singleton
-class VestibuleService {
+object VestibuleService {
+}
 
-  private var nextId = 1
+@Singleton
+class VestibuleService @Inject() (implicit system: ActorSystem) {
+  private implicit val timeout: akka.util.Timeout = 1 seconds
+  private val actor = VestibuleActor (this)
   var clock: () => Long = {() => System.currentTimeMillis ()}
 
-  private val data = ListBuffer[DisplayPlayer] ()
-
   def playerChallenge (meId: Int, himId: Int): Future[(Option[DisplayPlayer], Option[DisplayPlayer])] = {
-    Future {
-      val mePlayerOpt = data.find {p => p.id == meId}
-      val himPlayerOpt = data.find {p => p.id == himId}
-      if (mePlayerOpt.isDefined) {removePlayerById (mePlayerOpt.get.id)}
-      (mePlayerOpt, himPlayerOpt)
-    }
+    val response = (actor ? PlayerChallengeReq (meId, himId)).mapTo[PlayerChallengeResp]
+    response.map {r => (r.mePlayerOpt, r.himPlayerOpt)}
   }
 
   def waitingPlayers (): Future[List[DisplayPlayer]] = {
-    Future {
-      data.toList
-    }
+    val response = (actor ? WaitingPlayersReq ()).mapTo[WaitingPlayersResp]
+    response.map {r => r.players}
   }
 
   def playerEntered (name: String, representative: ActorRef): Future[Int] = {
-    Future {
-      val id = nextId
-      data.append (DisplayPlayer (id, name, clock (), representative))
-      nextId += 1
-      id
-    }
+    val response = (actor ? PlayerEnteredReq (name, representative)).mapTo[PlayerEnteredResp]
+    response.map {r => r.id}
   }
 
   def playerLeft (id: Int): Unit = {
-    removePlayerById (id)
+    actor ! PlayerLeftReq (id)
   }
 
-  private def removePlayerById (id: Int): Boolean = {
-    val (index, found) = data.foldLeft ((0, false)) {(soFar, elem) =>
-      soFar match {
-        case (idx, true) => (idx, true)
-        case (idx, false) if elem.id == id => (idx, true)
-        case (idx, false) => (idx + 1, false)
+  private case class PlayerChallengeReq (meId: Int, himId: Int)
+  private case class PlayerChallengeResp (mePlayerOpt: Option[DisplayPlayer], himPlayerOpt: Option[DisplayPlayer])
+  private case class WaitingPlayersReq ()
+  private case class WaitingPlayersResp (players: List[DisplayPlayer])
+  private case class PlayerEnteredReq (name: String, representative: ActorRef)
+  private case class PlayerEnteredResp (id: Int)
+  private case class PlayerLeftReq (id: Int)
+
+  object VestibuleActor {
+    def apply (service: VestibuleService)(implicit system: ActorSystem): ActorRef = {
+      system.actorOf (Props (classOf[VestibuleActor], service))
+    }
+  }
+
+  class VestibuleActor () extends Actor {
+
+    private var nextId = 1
+
+    private val data = ListBuffer[DisplayPlayer] ()
+
+    def receive = {
+      case msg: PlayerChallengeReq => playerChallenge (msg.meId, msg.himId)
+      case msg: WaitingPlayersReq => waitingPlayers ()
+      case msg: PlayerEnteredReq => playerEntered (msg.name, msg.representative)
+      case msg: PlayerLeftReq => playerLeft (msg.id)
+    }
+
+    private def playerChallenge (meId: Int, himId: Int): Unit = {
+      val mePlayerOpt = data.find {p => p.id == meId}
+      val himPlayerOpt = data.find {p => p.id == himId}
+      if (mePlayerOpt.isDefined) {removePlayerById (mePlayerOpt.get.id)}
+      sender ! PlayerChallengeResp (mePlayerOpt, himPlayerOpt)
+    }
+
+    private def waitingPlayers (): Unit = {
+      sender ! WaitingPlayersResp (data.toList)
+    }
+
+    private def playerEntered (name: String, representative: ActorRef): Unit = {
+      val id = nextId
+      data.append (DisplayPlayer (id, name, clock (), representative))
+      nextId += 1
+      sender ! PlayerEnteredResp (id)
+    }
+
+    private def playerLeft (id: Int): Unit = {
+      removePlayerById (id)
+    }
+
+    private def removePlayerById (id: Int): Boolean = {
+      val (index, found) = data.foldLeft ((0, false)) {(soFar, elem) =>
+        soFar match {
+          case (idx, true) => (idx, true)
+          case (idx, false) if elem.id == id => (idx, true)
+          case (idx, false) => (idx + 1, false)
+        }
       }
-    }
-    if (found) {
-      data.remove (index)
-      true
-    }
-    else {
-      false
+      if (found) {
+        data.remove (index)
+        true
+      }
+      else {
+        false
+      }
     }
   }
 }
